a680977abe5a5597a182e2dfb0c5838a4dcc1d49
rupinder-on-github <46348882+rupinder-on-github@users.noreply.github.com>
2023-08-13 23:21:04 -0400

    Update redisFailover.sh

--- redisFailover.sh
+++ redisFailover.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-while getopts "c:a:z:r:" opt; do
+while getopts "c:z:r:" opt; do
   case $opt in
     c) CLUSTER_NAME="$OPTARG";;
-    a) ACTION="$OPTARG";;
-    z) FAILOVER_AZ="$OPTARG";;
+    z) TARGET_AZ="$OPTARG";;
     r) REGION="$OPTARG";;
     \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
     :) echo "Option -$OPTARG requires an argument." >&2; exit 1;;
@@ -12,69 +11,59 @@ while getopts "c:a:z:r:" opt; do
 done
 
 # Check if required parameters are provided
-if [[ -z $CLUSTER_NAME || -z $ACTION || -z $FAILOVER_AZ || -z $REGION ]]; then
-  echo "Usage: $0 -c <cluster> -a <action> -z <az> -r <region>"
+if [[ -z $CLUSTER_NAME || -z $TARGET_AZ || -z $REGION ]]; then
+  echo "Usage: $0 -c <cluster> -z <target-az> -r <region>"
   exit 1
 fi
 
-if [[ $ACTION != "failover" && $ACTION != "failback" ]]; then
-  echo "Invalid action parameter. Use 'failover' or 'failback'."
-  exit 1
-fi
+PRIMARY_CLUSTER_ID=""
+REPLICA_AZ=""
 
-# Check if AZ is valid
-VALID_AZS=("us-east-2a" "us-east-2b" "us-east-2c")
-if [[ ! " ${VALID_AZS[@]} " =~ " $FAILOVER_AZ " ]]; then
-  echo "Invalid AZ parameter. Choose from: ${VALID_AZS[@]}"
+# Get a list of replicas and their Availability Zones
+REPLICA_LIST=$(aws elasticache describe-replication-groups \
+  --replication-group-id "$CLUSTER_NAME" \
+  --query "ReplicationGroup.NodeGroups[0].NodeGroupMembers[?CurrentRole=='primary'].PreferredAvailabilityZone" \
+  --output text \
+  --region "$REGION")
+
+# Iterate through the list of replicas to find the one in the target AZ
+while IFS= read -r replica; do
+  if [ "$replica" == "$TARGET_AZ" ]; then
+    PRIMARY_CLUSTER_ID=$(aws elasticache describe-replication-groups \
+      --replication-group-id "$CLUSTER_NAME" \
+      --query "ReplicationGroup.NodeGroups[0].NodeGroupMembers[?CurrentRole=='primary'].CacheClusterId" \
+      --output text \
+      --region "$REGION")
+    break
+  fi
+done <<< "$REPLICA_LIST"
+
+if [ -z "$PRIMARY_CLUSTER_ID" ]; then
+  echo "No primary instance found in target AZ $TARGET_AZ."
   exit 1
 fi
 
 try_failover() {
-  aws elasticache modify-replication-group \
+  aws elasticache move-replica \
     --replication-group-id "$CLUSTER_NAME" \
-    --replication-group-region "$REGION" \
-    --primary-replication-group-id "$CLUSTER_NAME-replica-$FAILOVER_AZ" \
-    --apply-immediately
+    --source-replica "$PRIMARY_CLUSTER_ID" \
+    --destination-availability-zone "$TARGET_AZ" \
+    --region "$REGION"
 
   if [ $? -ne 0 ]; then
     echo "Failed to initiate failover."
     exit 1
   fi
 
-  # Wait for the promotion to complete
-  echo "Waiting for the promotion to complete..."
+  # Wait for the failover to complete
+  echo "Waiting for failover to complete..."
   aws elasticache wait cache-cluster-available \
     --cache-cluster-id "$CLUSTER_NAME" \
     --region "$REGION"
 
-  echo "Failover complete. New primary in $FAILOVER_AZ."
+  echo "Failover complete. New primary in $TARGET_AZ."
 }
 
-try_failback() {
-  aws elasticache modify-replication-group \
-    --replication-group-id "$CLUSTER_NAME" \
-    --replication-group-region "$REGION" \
-    --primary-replication-group-id "$CLUSTER_NAME" \
-    --apply-immediately
-
-  if [ $? -ne 0 ]; then
-    echo "Failed to initiate failback."
-    exit 1
-  fi
-
-  # Wait for the failback to complete
-  echo "Waiting for failback to complete..."
-  aws elasticache wait cache-cluster-available \
-    --cache-cluster-id "$CLUSTER_NAME" \
-    --region "$REGION"
-
-  echo "Failback complete. Original primary restored."
-}
-
-if [ "$ACTION" = "failover" ]; then
-  try_failover
-elif [ "$ACTION" = "failback" ]; then
-  try_failback
-fi
+try_failover
 
 echo "Done!"
